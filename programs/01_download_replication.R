f1e71330a8ebdaff7a5b665dafa461a675a6947c
Lars Vilhuber <lars.vilhuber@cornell.edu>
2018-10-03 14:38:33 -0400

    Adding bib updates, programs

---
+++ programs/01_download_replication.R
@@ -0,0 +1,3 @@
+# This program downloads the data created by the replicators from the
+# Google sheets noted in the config.R
+# and saves the CSV / R/ DTA files in data/replications_data
---
+++ programs/config.R
@@ -0,0 +1,42 @@
+# ###########################
+# CONFIG: define paths and filenames for later reference
+# ###########################
+
+# Change the basepath depending on your system
+basepath <- ".."
+
+# Load packages
+# library(plyr)
+# library(data.table)
+# library(magrittr)
+# library(dplyr)
+# library(stargazer)
+
+# Main directories
+dataloc <- paste(basepath, "data/replication_data", sep="/")
+hindexloc <- paste(basepath, "data/h_index_data", sep="/")
+TexIncludes <- paste(basepath, "text/includes", sep="/")
+Outputs <- paste(basepath, "analysis", sep="/")
+
+# Questionnaire files
+ExitQ <- "Exit_Questionnaire_Draft (Responses) - Form Responses 1.csv"
+EntryQ1 <- "ReplicationDataQuestionnaire (Responses) - Form Responses 1.csv"
+EntryQ2 <- "Entry_Questionnaire_Draft (Responses) - Form Responses 1.csv"
+
+# Replication list files
+RepList2014 <- "Replication_List - First2014.csv"
+RepList2015a <- "Replication_List - First2015.csv"
+RepList2015b <- "Replication_List - Second2015.csv"
+RepList2016a <- "Replication_List - Spring2016.csv"
+RepList2016b <- "Replication_List -  Summer2016.csv"
+RepList2017 <- "Replication_List - Summer2017.csv"
+RepList2018a <- "Replication_List - Spring2018.csv"
+
+# H-index files
+HindexRaw <- "h-index-assignment1.csv"
+HindexClean <- "hindex.csv"
+
+
+
+
+
